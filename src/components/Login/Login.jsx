eb57dcf825fee6a35023a907bb75c4d979732027
Siddharth <siddharth201820@gmail.com>
2023-06-25 17:34:13 +0530

    responsive note

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,9 +2,9 @@ import RightSideBar from "./RightSideBar";
 
 const Login = () => {
     return (
-        <div className="lg:grid lg:grid-cols-8 h-screen">
+        <div className="lg:grid lg:grid-cols-8 min-h-screen">
             <div className="bg-black lg:col-span-3 lg:h-screen flex justify-center items-center">
-                <h1 className="p-4 text-white text-4xl font-extrabold md:text-6xl">
+                <h1 className="p-4 text-white text-3xl font-extrabold md:text-4xl">
                     Board.
                 </h1>
             </div>
